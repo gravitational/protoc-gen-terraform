6cc9ad3c86a1e15dafa198a8eb9db68db4f94b24
Victor Sokolov <gzigzigzeo@gmail.com>
2022-03-02 13:13:53 +0300

    Fixed wrong prioritizing for PlanModifiers

--- comments_test.go
+++ comments_test.go
@@ -6,12 +6,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	singleline Comment = "Test"
-	multiline  Comment = "Test\n Line 2"
-)
-
 func TestToSingleLine(t *testing.T) {
-	require.Equal(t, "Test", singleline.ToSingleLine())
-	require.Equal(t, "Test Line 2", multiline.ToSingleLine())
+	test := func(input, expected string) func(t *testing.T) {
+		return func(t *testing.T) {
+			require.Equal(t, expected, Comment(input).ToSingleLine())
+		}
+	}
+
+	t.Run("Single Line", test("Test", "Test"))
+	t.Run("Multiline Line", test("Test\n Line 2", "Test Line 2"))
+	t.Run("Empty String", test("", ""))
+	t.Run("CRLF", test("Test\r\n Line 2", "Test Line 2"))
+	t.Run("Leading", test("\nLine 1\n Line 2", "Line 1 Line 2"))
+	t.Run("Trailing", test("Line 1\n Line 2\n", "Line 1 Line 2"))
+	t.Run("Trailing", test("Line 1\n   Line 2\n", "Line 1 Line 2"))
 }
