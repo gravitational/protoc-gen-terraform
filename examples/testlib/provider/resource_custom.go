fa10d698666a5ca601ef9c02dc6b0e6c643c4689
Bernard Kim <bernard@goteleport.com>
2026-08-20 12:17:09 -0700

    Fix example provider and resource implementation
    - Metadata is now required
    - Schema is now required

--- examples/testlib/provider/resource_custom.go
+++ examples/testlib/provider/resource_custom.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	schemav1 "github.com/gravitational/protoc-gen-terraform/v4/examples/tfschema/custom/v1"
@@ -24,8 +23,10 @@ func (r customResource) Metadata(ctx context.Context, req resource.MetadataReque
 	resp.TypeName = "example_custom"
 }
 
-func (r customResource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
-	return schemav1.GenSchemaCustom(ctx)
+func (r customResource) Schema(ctx context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	schema, diags := schemav1.GenSchemaCustom(ctx)
+	resp.Schema = schema
+	resp.Diagnostics.Append(diags...)
 }
 
 func (r customResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
--- examples/testlib/provider/resource_primitives.go
+++ examples/testlib/provider/resource_primitives.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	schemav1 "github.com/gravitational/protoc-gen-terraform/v4/examples/tfschema/primitives/v1"
@@ -25,8 +24,10 @@ func (r primitivesResource) Metadata(ctx context.Context, req resource.MetadataR
 	resp.TypeName = "example_primitives"
 }
 
-func (r primitivesResource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
-	return schemav1.GenSchemaPrimitives(ctx)
+func (r primitivesResource) Schema(ctx context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	schema, diags := schemav1.GenSchemaPrimitives(ctx)
+	resp.Schema = schema
+	resp.Diagnostics.Append(diags...)
 }
 
 func (r primitivesResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
