6836fd7b231ebf5d99701d9f6a83350c5da48326
Bernard Kim <bernard@goteleport.com>
2026-09-08 09:58:17 -0700

    Upgrade terraform-plugin-framework to v1.0.1
    v1.0.1 prevents mutation of ObjectValue.Attributes method return

--- examples/testlib/provider/resource_primitives.go
+++ examples/testlib/provider/resource_primitives.go
@@ -42,7 +42,13 @@ func (r primitivesResource) Create(ctx context.Context, req resource.CreateReque
 		resp.Diagnostics.Append(diag.NewErrorDiagnostic("unable to generate uuid", err.Error()))
 	}
 
-	plan.Attributes()["id"] = types.StringValue(id)
+	attrs := plan.Attributes()
+	attrs["id"] = types.StringValue(id)
+	plan, diags := types.ObjectValue(plan.AttributeTypes(ctx), attrs)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
 	primitives := &extypes.Primitives{}
 	resp.Diagnostics.Append(schemav1.CopyPrimitivesFromTerraform(ctx, plan, primitives)...)
